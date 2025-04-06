55acb65a43908dd50adef3661ca3c6d6289f1036
echangepro <julesdelcausse@gmail.com>
2025-04-06 15:46:10 +0200

    Add files via upload

---
+++ nexovo_github_site_final/assets/js/main.js
@@ -0,0 +1,12 @@
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+document.querySelectorAll('.hidden').forEach(section => {
+  observer.observe(section);
+});
